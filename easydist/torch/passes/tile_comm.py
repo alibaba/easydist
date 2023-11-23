5c9d960aa79a53b8b78fc22fa2514a57b1b01b76
Shenggan <csg19971016@gmail.com>
2023-11-23 06:56:20 +0000

    [torch] add tile solver

--- easydist/torch/passes/tile_comm.py
+++ easydist/torch/passes/tile_comm.py
@@ -13,10 +13,250 @@
 # ==============================================================================
 
 import logging
+from dataclasses import dataclass, field
+from typing import List, Dict
 
+import mip
+import rich
 import torch
+import torch.utils._pytree as pytree
+
+from easydist import mdconfig
+from easydist.metashard.combination import CombinationFunc
+from .sharding import all_gather_start, all_reduce_start
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class TiledNode:
+    node: torch.fx.Node
+    tile_axis: int
+    num_tiles: int
+
+
+@dataclass
+class TileStrategy:
+    num_tiles: int
+    tms_comm: float = 0.0
+    tms_prev_comp: float = 0.0
+    tms_post_comp: float = 0.0
+    tiled_comm_node: List[TiledNode] = None
+    tiled_prev_node: List[TiledNode] = field(default_factory=list)
+    tiled_post_node: List[TiledNode] = field(default_factory=list)
+
+    def append_prev_node(self, tiled_node: TiledNode):
+        if self.tiled_prev_node is None:
+            self.tiled_prev_node = []
+        self.tiled_prev_node.append(tiled_node)
+        self.tms_prev_comp += tiled_node.node.ed_info.runtime_ms
+
+    def append_post_node(self, tiled_node: TiledNode):
+        if self.tiled_post_node is None:
+            self.tiled_post_node = []
+        self.tiled_post_node.append(tiled_node)
+        self.tms_post_comp += tiled_node.node.ed_info.runtime_ms
+
+
+def forward_tiled_node(tiled_node: TiledNode, user: torch.fx.Node) -> TiledNode:
+    flatten_args, _ = pytree.tree_flatten(user.args)
+    flatten_tensor_args = [i for i in flatten_args if isinstance(i, torch.fx.Node)]
+
+    arg_index = flatten_tensor_args.index(tiled_node.node)
+    shard_dim = user.ed_info.spmd_annotation['sharding_ann'][arg_index][tiled_node.tile_axis]
+    # is not NoShardDim
+    if shard_dim.shard_dim_id != 0:
+        out_spmd = user.ed_info.spmd_annotation['combination_ann'][shard_dim.shard_dim_id]
+        if out_spmd.func == CombinationFunc.gather:
+            return TiledNode(user, out_spmd.keywords['dim'], tiled_node.num_tiles)
+
+    return None
+
+
+def backward_tiled_node(tiled_node: TiledNode, arg: torch.fx.Node) -> TiledNode:
+
+    if tiled_node.node.target in [all_gather_start, all_reduce_start]:
+        return TiledNode(arg, tiled_node.tile_axis, tiled_node.num_tiles)
+
+    flatten_args, _ = pytree.tree_flatten(tiled_node.node.args)
+    flatten_tensor_args = [i for i in flatten_args if isinstance(i, torch.fx.Node)]
+    arg_index = flatten_tensor_args.index(arg)
+
+    for shard_dim_id, combine_func in tiled_node.node.ed_info.spmd_annotation[
+            'combination_ann'].items():
+        if combine_func.func == CombinationFunc.gather and combine_func.keywords[
+                'dim'] == tiled_node.tile_axis:
+            arg_spmd = tiled_node.node.ed_info.spmd_annotation['sharding_ann'][arg_index]
+            arg_tile_axis = [i.shard_dim_id for i in arg_spmd].index(shard_dim_id)
+            return TiledNode(arg, arg_tile_axis, tiled_node.num_tiles)
+
+    return None
+
+
+class TileSolver:
+
+    def __init__(self, strategy_for_all: Dict[torch.fx.Node, List[TileStrategy]]) -> None:
+        self.strategy_for_all = strategy_for_all
+        self.m = mip.Model("tile-solver")
+
+        self.node = {}
+        self.tile_cost = 0
+
+        for c_node in self.strategy_for_all:
+            node_prev_strategy, node_post_strategy = [], []
+            cnode_axis_prev_strategy, cnode_axis_post_strategy = [], []
+
+            prev_tile_space, post_tile_space = [], []
+
+            for strategy in self.strategy_for_all[c_node]:
+                for prev_node in strategy.tiled_prev_node:
+                    prev_tile_space.append(prev_node.node.name)
+                for post_node in strategy.tiled_post_node:
+                    post_tile_space.append(post_node.node.name)
+
+                for i in range(len(strategy.tiled_prev_node) + 1):
+                    node_prev_strategy.append(strategy.tiled_prev_node[:i])
+                    cnode_axis_prev_strategy.append(strategy.tiled_comm_node[0].tile_axis)
+
+                for i in range(len(strategy.tiled_post_node) + 1):
+                    node_post_strategy.append(strategy.tiled_post_node[:i])
+                    cnode_axis_post_strategy.append(strategy.tiled_comm_node[0].tile_axis)
+
+            self.node[c_node] = {
+                'prev':
+                node_prev_strategy,
+                'post':
+                node_post_strategy,
+                'cnode_axis_prev_strategy':
+                cnode_axis_prev_strategy,
+                'cnode_axis_post_strategy':
+                cnode_axis_post_strategy,
+                'prev_tile_space':
+                set(prev_tile_space),
+                'post_tile_space':
+                set(post_tile_space),
+                'prev_mip_var':
+                [self.m.add_var(var_type=mip.BINARY) for _ in range(len(node_prev_strategy))],
+                'post_mip_var':
+                [self.m.add_var(var_type=mip.BINARY) for _ in range(len(node_post_strategy))],
+            }
+            shape_1 = len(self.node[c_node]['prev'])
+            shape_2 = len(self.node[c_node]['post'])
+
+            self.m += mip.xsum(self.node[c_node]['prev_mip_var'][i] for i in range(shape_1)) == 1
+            self.m += mip.xsum(self.node[c_node]['post_mip_var'][i] for i in range(shape_2)) == 1
+
+            mip_matrix = [[self.m.add_var(var_type=mip.BINARY) for _ in range(shape_2)]
+                          for _ in range(shape_1)]
+
+            for i in range(shape_1):
+                for j in range(shape_2):
+                    self.m += mip_matrix[i][j] <= self.node[c_node]['prev_mip_var'][i]
+                    self.m += mip_matrix[i][j] <= self.node[c_node]['post_mip_var'][j]
+                    self.m += mip_matrix[i][j] >= self.node[c_node]['prev_mip_var'][i] + self.node[
+                        c_node]['post_mip_var'][j] - 1
+
+            cost_matrix = self.get_cost_matrix(c_node, node_prev_strategy, node_post_strategy,
+                                               cnode_axis_prev_strategy, cnode_axis_post_strategy)
+            # add tile cost for each communication node
+            self.tile_cost += mip.xsum(mip_matrix[i][j] * cost_matrix[i][j] for i in range(shape_1)
+                                       for j in range(shape_2))
+
+            # add tile cost between communication node who share the same tile node
+            for node in self.node:
+                if node == c_node:
+                    continue
+                if any(i in self.node[node]['prev_tile_space'] for i in self.node[c_node]['prev_tile_space']):
+                    self.add_edge(c_node, node, 'prev', 'prev')
+                if any(i in self.node[node]['prev_tile_space'] for i in self.node[c_node]['post_tile_space']):
+                    self.add_edge(c_node, node, 'post', 'prev')
+                if any(i in self.node[node]['post_tile_space'] for i in self.node[c_node]['prev_tile_space']):
+                    self.add_edge(c_node, node, 'prev', 'post')
+                if any(i in self.node[node]['post_tile_space'] for i in self.node[c_node]['post_tile_space']):
+                    self.add_edge(c_node, node, 'post', 'post')
+
+        self.m.objective = mip.minimize(self.tile_cost)
+
+    def add_edge(self, c_node, node, cnode_edge, node_edge):
+
+        shape_1 = len(self.node[c_node][cnode_edge])
+        c_node_mip_var_key = 'prev_mip_var' if cnode_edge == 'prev' else 'post_mip_var'
+        c_node_mip_var = self.node[c_node][c_node_mip_var_key]
+        shape_2 = len(self.node[node][node_edge])
+        node_mip_var_key = 'prev_mip_var' if node_edge == 'prev' else 'post_mip_var'
+        node_mip_var = self.node[node][node_mip_var_key]
+
+        mip_matrix = [[self.m.add_var(var_type=mip.BINARY) for _ in range(shape_2)]
+                       for _ in range(shape_1)]
+
+        for i in range(shape_1):
+            for j in range(shape_2):
+                self.m += mip_matrix[i][j] <= c_node_mip_var[i]
+                self.m += mip_matrix[i][j] <= node_mip_var[j]
+                self.m += mip_matrix[i][j] >= c_node_mip_var[i] + node_mip_var[j] - 1
+
+        cost_matrix = self.get_edge_cost_matrix(self.node[c_node][cnode_edge], self.node[node][node_edge])
+
+        self.tile_cost += mip.xsum(mip_matrix[i][j] * cost_matrix[i][j] for i in range(shape_1)
+                                       for j in range(shape_2))
+
+    def get_cost_matrix(self, node, node_prev_strategy, node_post_strategy,
+                        cnode_axis_prev_strategy, cnode_axis_post_strategy):
+        runtime_ms = node.ed_info.runtime_ms
+        cost_matrix = [[runtime_ms for _ in range(len(node_post_strategy))]
+                       for _ in range(len(node_prev_strategy))]
+        for i in range(len(node_prev_strategy)):
+            for j in range(len(node_post_strategy)):
+                # reduce the cost when only tile prev or post, or same tile axis for prev/post
+                if len(node_prev_strategy[i]) == 0 or len(
+                        node_post_strategy[j]
+                ) == 0 or cnode_axis_prev_strategy[i] == cnode_axis_post_strategy[j]:
+                    for tiled_node in node_prev_strategy[i]:
+                        num_tiles = tiled_node.num_tiles
+                        cost_matrix[i][j] -= (1 + mdconfig.nvlink_processor_usage) * 2 * (
+                            num_tiles - 1) / num_tiles * tiled_node.node.ed_info.runtime_ms
+                    for tiled_node in node_post_strategy[j]:
+                        num_tiles = tiled_node.num_tiles
+                        cost_matrix[i][j] -= (1 + mdconfig.nvlink_processor_usage) * 2 * (
+                            num_tiles - 1) / num_tiles * tiled_node.node.ed_info.runtime_ms
+
+        for i in range(len(node_prev_strategy)):
+            for j in range(len(node_post_strategy)):
+                cost_matrix[i][j] = max(cost_matrix[i][j], 0)
+
+        return cost_matrix
+
+    def get_edge_cost_matrix(self, cnode_edge, node_edge):
+        cost_matrix = [[0 for _ in range(len(node_edge))] for _ in range(len(cnode_edge))]
+
+        for i in range(len(cnode_edge)):
+            for j in range(len(node_edge)):
+                for cnode_tiled_node in cnode_edge[i]:
+                    for node_tiled_node in node_edge[j]:
+                        if cnode_tiled_node.node.name == node_tiled_node.node.name:
+                            num_tiles = cnode_tiled_node.num_tiles
+                            if cnode_tiled_node.tile_axis == node_tiled_node.tile_axis:
+                                cost_matrix[i][j] -= (mdconfig.nvlink_processor_usage + 1) * (num_tiles - 2) / num_tiles * cnode_tiled_node.node.ed_info.runtime_ms
+                            else:
+                                cost_matrix[i][j] -= (mdconfig.nvlink_processor_usage + 1) * (num_tiles - 1) / num_tiles * cnode_tiled_node.node.ed_info.runtime_ms
+
+        return cost_matrix
+
+    def solve(self):
+        self.m.verbose = 0
+        status = self.m.optimize()
+        logger.info(f'[TileSolver.status]:\t {status}')
+        logger.info(f'[TileSolver.solution_cost]:\t {self.m.objective_value}')
+
+        final_strategy = {}
+
+        for c_node in self.node:
+            final_strategy[c_node] = TileStrategy(
+                num_tiles=2,
+                tms_comm=c_node.ed_info.runtime_ms,
+                tiled_comm_node=self.node[c_node]['tiled_comm_node'])
+
+        return final_strategy
 
 
 def tile_comm(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
@@ -53,41 +293,132 @@ def tile_comm(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
             post_nodes.extend(_cache_post_node[user])
         return post_nodes
 
-    for node in fx_module.graph.nodes:
-        if node.op == 'call_function':
-            if node.ed_info.is_communication():
-                communication_nodes.append(node)
-            else:
-                computation_nodes.append(node)
+    # solve tile strategy on the rank 0
+    if torch.distributed.get_rank() == 0:
+        for node in fx_module.graph.nodes:
+            if node.op == 'call_function':
+                if node.ed_info.is_communication():
+                    communication_nodes.append(node)
+                else:
+                    computation_nodes.append(node)
 
-    # Step 2: find the communication nodes that are critical
-    critical_communication = {}
-    for node in fx_module.graph.nodes:
-        if node.op == 'call_function' and node.ed_info.is_communication():
-            prev_nodes = set(find_all_prev_nodes(node))
-            post_nodes = set(find_all_post_nodes(node))
-            independent_nodes = set(computation_nodes) - \
-                prev_nodes - post_nodes
+        fx_module.print_readable()
 
-            independent_nodes_runtime = 0.0
-            for indpd_node in independent_nodes:
-                independent_nodes_runtime += indpd_node.ed_info.runtime_ms
+        # Step 2: find the communication nodes that are critical
+        critical_communication = {}
+        for node in fx_module.graph.nodes:
+            if node.op == 'call_function' and node.ed_info.is_communication():
+                prev_nodes = set(find_all_prev_nodes(node))
+                post_nodes = set(find_all_post_nodes(node))
+                independent_nodes = set(computation_nodes) - \
+                    prev_nodes - post_nodes
 
-            node_info = f"[{node.name}] pre={len(prev_nodes)} post={len(post_nodes)} indpd={len(independent_nodes)}"
+                independent_nodes_runtime = 0.0
+                for indpd_node in independent_nodes:
+                    independent_nodes_runtime += indpd_node.ed_info.runtime_ms
 
-            logger.debug(
-                f"{node_info} runtime={node.ed_info.runtime_ms:.4f}/{independent_nodes_runtime:.4f}"
-            )
+                node_info = f"[{node.name}] pre={len(prev_nodes)} post={len(post_nodes)} indpd={len(independent_nodes)}"
 
-            # TODO need to extend if independent_nodes can not cover the communication
-            if independent_nodes_runtime <= node.ed_info.runtime_ms:
-                critical_communication[node] = None
+                logger.info(
+                    f"{node_info} runtime={node.ed_info.runtime_ms:.4f}/{independent_nodes_runtime:.4f}"
+                )
 
-    logger.info(
-        f"Number of critical communication nodes: {len(critical_communication)}")
+                # TODO need to extend if independent_nodes can not cover the communication
+                if independent_nodes_runtime <= node.ed_info.runtime_ms:
+                    critical_communication[node] = None
 
-    # Step 3: determine the strategy for each critical communication node
+        logger.info(f"Number of critical communication nodes: {len(critical_communication)}")
 
-    # Step 4: tile the critical communication nodes and the computation context
+        # (TODO) here we assume the number of tiles is 2
+        num_tiles = 2
+
+        all_tile_strategies = {}
+
+        # Step 3: determine the strategy for each critical communication node
+        for c_node in critical_communication:
+            if c_node.target in [all_gather_start, all_reduce_start]:
+                comm_tensor_shape = c_node.prev.meta['val'].shape
+                tile_dim_list = list(range(len(comm_tensor_shape)))
+
+                if c_node.target == all_gather_start:
+                    gather_dim = c_node.args[1]
+                    del tile_dim_list[gather_dim]
+
+                tms_comm = c_node.ed_info.runtime_ms
+                # (TODO) nvlink and ib have different processor_usage
+                min_comp_needed = tms_comm / (1 + mdconfig.nvlink_processor_usage) / (num_tiles -
+                                                                                      1)
+                max_comp_needed = 0.5 * min_comp_needed * num_tiles - min_comp_needed
+                max_comp_needed = max(max_comp_needed, min_comp_needed)
+
+                c_end_node = list(c_node.users.keys())[0]
+
+                # explore all the possible tile axis
+                for tile_dim in tile_dim_list:
+                    node_tile_strategy = TileStrategy(num_tiles=num_tiles,
+                                                      tms_comm=tms_comm,
+                                                      tiled_comm_node=[
+                                                          TiledNode(c_node, tile_dim, num_tiles),
+                                                          TiledNode(c_end_node, tile_dim,
+                                                                    num_tiles)
+                                                      ])
+
+                    # BFS to find the previous computation nodes that can be tiled
+                    prev_node_queue = [TiledNode(c_node, tile_dim, num_tiles)]
+                    while len(prev_node_queue
+                              ) > 0 and node_tile_strategy.tms_prev_comp < max_comp_needed:
+                        tiled_node = prev_node_queue.pop(0)
+                        for arg in tiled_node.node.args:
+                            if isinstance(arg, torch.fx.Node):
+                                if arg.op == 'placeholder':
+                                    continue
+                                if arg.ed_info.is_computation():
+
+                                    # backward the tile axis to the previous computation node
+                                    tiled_arg = backward_tiled_node(tiled_node, arg)
+
+                                    node_tile_strategy.append_prev_node(tiled_arg)
+                                    prev_node_queue.append(tiled_arg)
+                                    print(tiled_arg, node_tile_strategy.tms_prev_comp)
+
+                            if node_tile_strategy.tms_prev_comp >= max_comp_needed:
+                                break
+
+                    # BFS to find the post computation nodes that can be tiled
+                    post_node_queue = [TiledNode(c_end_node, tile_dim, num_tiles)]
+                    while len(post_node_queue
+                              ) > 0 and node_tile_strategy.tms_post_comp < max_comp_needed:
+                        tiled_node = post_node_queue.pop(0)
+                        for user in tiled_node.node.users:
+                            if user.op == 'output':
+                                continue
+                            if user.ed_info.is_computation():
+
+                                # forward the tile axis to the post computation node
+                                tiled_user = forward_tiled_node(tiled_node, user)
+
+                                if tiled_user is not None:
+                                    node_tile_strategy.append_post_node(tiled_user)
+                                    post_node_queue.append(tiled_user)
+                                    print(tiled_user, node_tile_strategy.tms_post_comp)
+
+                            if node_tile_strategy.tms_post_comp >= max_comp_needed:
+                                break
+
+                    if not c_node in all_tile_strategies:
+                        all_tile_strategies[c_node] = []
+                    all_tile_strategies[c_node].append(node_tile_strategy)
+
+        rich.print(all_tile_strategies)
+        # Step 4: solve the tile strategy with mip
+        final_ = TileSolver(all_tile_strategies).solve()
+
+        import pdb
+        pdb.set_trace()
+
+    # broadcast the tile strategy to all the ranks
+    # Step 5: tile the critical communication nodes and the computation context
+
+    torch.distributed.barrier()
 
     return fx_module
