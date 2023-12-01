692c0945f13e936916eeef6bfa0226ee2c082692
botbw <wang1570@e.ntu.edu.sg>
2023-12-01 17:19:26 +0800

    refactor

--- tests/test_torch/pp/test_compile.py
+++ tests/test_torch/pp/test_compile.py
@@ -1,27 +1,19 @@
-import argparse
-import copy
 import logging
 import os
-from contextlib import nullcontext
+import numpy
 
 import torch
 from torch.distributed._tensor import DeviceMesh
-from torch._subclasses.fake_tensor import FakeTensorMode
-from torch.distributed.distributed_c10d import _get_default_group
-from torch.distributed.utils import _sync_module_states
-from torch.utils.checkpoint import checkpoint
-from easydist.torch.experimental.pp.IR import (compile_symbolic_splited, run_local_split_gm, symbolic_split)
+from easydist.torch.experimental.pp.IR import symbolic_split
+from easydist.torch.experimental.pp.compile_splited import compile_splited
 from easydist.torch.experimental.pp.split_policies import split_into_equal_size
-from easydist.torch.device_mesh import (device_mesh_world_size, get_device_mesh, set_device_mesh)
+from easydist.torch.device_mesh import (get_device_mesh, set_device_mesh)
 
 from easydist import easydist_setup, mdconfig
-from easydist.torch.api import easydist_compile
+from easydist.torch.compiler import easydist_shard
+
 from torchvision.models import alexnet, resnet18, vgg19
 
-
-def save_dot(dot, fname):
-    with open(fname, 'w') as f:
-        f.write(dot.source)
 
 class Foo(torch.nn.Module):
 
@@ -37,29 +29,56 @@ class Foo(torch.nn.Module):
         return self.relu(linear)
 
 
+signature = 0x3f3f3f3f
+
+
 def test_compile(model_class, input_size):
     model = model_class().cuda().train()
     rand_input = torch.rand(input_size).cuda()
-
     split_policy = split_into_equal_size(2)
-    model_split = symbolic_split(model, split_policy=split_policy)
+    model_split, _ = symbolic_split(model, split_policy=split_policy)
+    compiled_splited = compile_splited(model_split, rand_input)
 
-    compile_symbolic_splited(model_split, rand_input)
+    out = compile_forward(compiled_splited, rand_input)
+    if not isinstance(out, tuple):
+        out = (out, )
+    compile_backward(compiled_splited, [torch.ones_like(t) for t in out])
 
-    from easydist.torch.compiler import easydist_shard
-    traced_graph = model_split.submod_0.fw_gm
-    state_tensor_num = len(model_split.submod_0.named_buffers) + len(model_split.submod_0.named_params)
-    input_signature = 102313124124
-    params = model_split.submod_0.named_params
-    buffers = model_split.submod_0.named_buffers
-    named_states = {
-        **params,
-        **buffers
-    }
+
+def compile_forward(compiled, rand_input):
+    global signature
     args = (rand_input, )
     kwargs = {}
-    ret = easydist_shard(traced_graph, state_tensor_num, input_signature, params, buffers, args, kwargs)
-    print(ret)
+    for name, (compiled_fw, _) in compiled.compiled_submods().items():
+        gm = compiled_fw.fw_gm
+        params = compiled_fw.named_params
+        buffers = compiled_fw.named_buffers
+        states = {**params, **buffers}
+        signature += 1
+        easydist_shard(gm, len(states), signature, params, buffers, args, kwargs)
+        args = compiled_fw(*args)
+        if not isinstance(args, tuple):
+            args = (args, )
+    return args
+
+
+def compile_backward(compiled, out_grads):
+    global signature
+    kwargs = {}
+    for name, (_, compiled_bw) in reversed(compiled.compiled_submods().items()):
+        args = compiled._get_args(out_grads, compiled_bw)
+        args = list(compiled_bw.compiled_fw.saved_tensors) + args
+        gm = compiled_bw.bw_gm
+        params = {}
+        buffers = {}
+        states = {**params, **buffers}
+        signature += 1
+        easydist_shard(gm, len(states), signature, params, buffers, args, kwargs)
+        out_all = gm(*args)
+        out_grads = out_all[len(compiled_bw.compiled_fw.named_params) +
+                            len(compiled_bw.compiled_fw.named_buffers):]
+    return out_grads
+
 
 if __name__ == "__main__":
     mdconfig.log_level = logging.INFO
@@ -68,13 +87,11 @@ if __name__ == "__main__":
     world_size = int(os.environ["WORLD_SIZE"])
     local_rank = int(os.environ["LOCAL_RANK"])
     torch.cuda.set_device(local_rank)
-    
-    import numpy
     mesh_shape = numpy.array(range(world_size)).reshape(1, -1)
     mesh = DeviceMesh("cuda", mesh_shape.tolist())
-
     if get_device_mesh() == None:
         set_device_mesh(mesh)
+
     imagenet_input_size = (16, 3, 224, 224)
     test_compile(Foo, (16, 1024))
     test_compile(resnet18, imagenet_input_size)
--- tests/test_torch/pp/test_split.py
+++ tests/test_torch/pp/test_split.py
@@ -5,8 +5,8 @@ import numpy as np
 import torch
 from torchvision.models import alexnet, resnet18, vgg19
 
-from easydist.torch.experimental.pp.IR import (
-    compile_symbolic_splited, symbolic_split)
+from easydist.torch.experimental.pp.IR import symbolic_split
+from easydist.torch.experimental.pp.compile_splited import compile_splited
 from easydist.torch.experimental.pp.split_policies import split_into_equal_size
 
 
@@ -28,6 +28,7 @@ def seed(seed=42):
 def loss_fn(x):
     return x.mean()
 
+
 class Foo(torch.nn.Module):
 
     def __init__(self):
@@ -42,51 +43,58 @@ class Foo(torch.nn.Module):
         return self.relu(linear)
 
 
-def test_split(model_class, input_size):
+def test_main(model_class, input_size):
     model = model_class().cuda().train()
     model_copy = copy.deepcopy(model)
 
     rand_input1 = torch.rand(input_size).cuda()
     rand_input2 = torch.rand(input_size).cuda()
 
-    out_torch, loss_torch, buffers_torch, params_torch, grads_torch = run_torch(model, rand_input1, rand_input2)
+    out_torch, loss_torch, buffers_torch, params_torch, grads_torch = run_torch(
+        model, rand_input1, rand_input2)
 
-    out_split, loss_split, buffers_split, params_split, grads_split = run_split(model_copy, rand_input1, rand_input2)
+    out_split, loss_split, buffers_split, params_split, grads_split = run_split(
+        model_copy, rand_input1, rand_input2)
 
+    # TODO @botbw: precision loss?
     assert torch.allclose(out_split, out_torch, atol=1e-4)
     assert torch.allclose(loss_split, loss_torch, atol=1e-4)
     assert len(buffers_split) == len(buffers_torch) and all(
         torch.allclose(b1, b2, atol=1e-4) for b1, b2 in zip(buffers_split, buffers_torch))
     assert len(grads_split) == len(grads_torch) and all(
-        torch.allclose(g1, g2, atol=1e-4) for g1, g2 in zip(grads_split, grads_torch))  # TODO @botbw: cannot pass with atol=1e-5
+        torch.allclose(g1, g2, atol=1e-4) for g1, g2 in zip(grads_split, grads_torch))
     assert len(params_split) == len(params_torch) and all(
         torch.allclose(p1, p2, atol=1e-4) for p1, p2 in zip(params_split, params_torch))
+
 
 def run_split(model_copy, rand_input1, rand_input2):
     opt_splited = torch.optim.SGD(model_copy.parameters(), lr=0.01)
     split_policy = split_into_equal_size(2)
     model_splited, _ = symbolic_split(model_copy, split_policy=split_policy)
-    compiled_splited = compile_symbolic_splited(model_splited, rand_input1)
+    compiled_splited = compile_splited(model_splited, rand_input1)
 
     seed()
     # step1
     out_split = compiled_splited.forward(rand_input1)
     loss_split = loss_fn(out_split)
-    out_grads = torch.autograd.grad(loss_split, [t for t in compiled_splited.raw_returns() if t.requires_grad])
+    out_grads = torch.autograd.grad(loss_split,
+                                    [t for t in compiled_splited.raw_returns() if t.requires_grad])
     compiled_splited.backward(out_grads)
     opt_splited.step()
     # step2
     opt_splited.zero_grad()
     out_split = compiled_splited.forward(rand_input2)
     loss_split = loss_fn(out_split)
-    out_grads = torch.autograd.grad(loss_split, [t for t in compiled_splited.raw_returns() if t.requires_grad])
+    out_grads = torch.autograd.grad(loss_split,
+                                    [t for t in compiled_splited.raw_returns() if t.requires_grad])
     compiled_splited.backward(out_grads)
     opt_splited.step()
 
     buffer_split = [t for t in compiled_splited.named_buffers().values()]
     param_split = [t for t in compiled_splited.named_parameters().values()]
     grad_split = [t.grad for t in compiled_splited.named_parameters().values()]
-    return out_split,loss_split,buffer_split,param_split,grad_split
+    return out_split, loss_split, buffer_split, param_split, grad_split
+
 
 def run_torch(model, rand_input1, rand_input2):
     opt = torch.optim.SGD(model.parameters(), lr=0.01)
@@ -103,17 +111,17 @@ def run_torch(model, rand_input1, rand_input2):
     loss_torch = loss_fn(out_torch)
     loss_torch.backward()
     opt.step()
-    
+
     buffers_torch = [t for t in model.buffers()]
     params_torch = [t for t in model.parameters()]
     grads_torch = [t.grad for t in model.parameters()]
-    return out_torch,loss_torch,buffers_torch,params_torch,grads_torch
+    return out_torch, loss_torch, buffers_torch, params_torch, grads_torch
 
 
 if __name__ == "__main__":
-    imagenet_input_size = (16, 3, 224, 224)
-    test_split(Foo, (16, 1024))
-    test_split(resnet18, imagenet_input_size)
-    test_split(vgg19, imagenet_input_size)
-    test_split(alexnet, imagenet_input_size)
+    imgnet = (16, 3, 224, 224)
+    test_main(Foo, (16, 1024))
+    test_main(resnet18, imgnet)
+    test_main(vgg19, imgnet)
+    test_main(alexnet, imgnet)
     print("passed")
