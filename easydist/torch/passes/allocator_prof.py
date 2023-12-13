68f181a6cc5adc48f48bc33e6a05633aa7192875
kuanzhan.wh <kuanzhan.wh@alibaba-inc.com>
2023-12-13 14:49:10 +0800

    [profiling_allocator] refactored NodeProfilingInfo

--- easydist/torch/passes/allocator_prof.py
+++ easydist/torch/passes/allocator_prof.py
@@ -31,6 +31,25 @@ from easydist.torch.graph_profile_db import PerfDB
 
 logger = logging.getLogger(__name__)
 
+class NodeProfilingInfo:
+    def __init__(self):
+        self.qualified_name = ""
+        self.input_address = []
+        self.output_address = []
+        self.allocator_address = []
+
+    def set_qualified_name(self, qualified_name):
+        self.qualified_name = qualified_name
+
+    def add_input_address(self, input_address):
+        self.input_address.append(input_address)
+
+    def add_output_address(self, output_address):
+        self.output_address.append(output_address)
+
+    def add_allocator_address(self, allocator_address):
+        self.allocator_address.append(allocator_address)
+
 def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
     # only profile on rank 0
     local_rank = int(os.environ["LOCAL_RANK"])
@@ -55,20 +74,19 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
             continue
 
         # create dict to store addresses for this node
-        node_profiling_info = dict()
+        node_profiling_info = NodeProfilingInfo()
 
         # record qualified names
-        node_profiling_info['qualified_name'] = qualified_name
+        node_profiling_info.set_qualified_name(qualified_name)
 
         # materialize inputs
         inputs_signature = pytree.tree_map_only(torch.fx.Node, lambda n: n.meta['val'], node.args)
         materialized_inputs = pytree.tree_map_only(torch.Tensor, materialize_random, inputs_signature)
 
         # record input addresses
-        node_profiling_info['input_address'] = []
         for materialized_input in materialized_inputs:
             if isinstance(materialized_input, torch.Tensor):
-                node_profiling_info['input_address'].append(materialized_input.data_ptr())
+                node_profiling_info.add_input_address(materialized_input.data_ptr())
         
         # set op_name for profiling_allocator.so
         __main__.op_name = node.name
@@ -80,18 +98,17 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
         node_output = node.target(*materialized_inputs, **node.kwargs)
 
         # record output addresses
-        node_profiling_info['output_address'] = []
 
         # if output is a single tensor
         if isinstance(node_output, torch.Tensor):
             out_tensor = node_output
-            node_profiling_info['output_address'].append(out_tensor.data_ptr())
+            node_profiling_info.add_output_address(out_tensor.data_ptr())
 
         # if output is a tuple of tensors
         elif isinstance(node_output, tuple):
             for out_item in node_output:
                 if isinstance(out_item, torch.Tensor):
-                    node_profiling_info['output_address'].append(out_item.data_ptr())
+                    node_profiling_info.add_output_address(out_item.data_ptr())
                 elif out_item is None:
                     continue
                 else:
@@ -99,10 +116,6 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
         else:
             assert False, "Unexpected type of out_tensors!"
         
-        # record allocator addresses
-        # for now, do nothing. we will write addresses later.
-        node_profiling_info['allocator_address'] = []
-
         # tell profiling_allocator stop recording
         __main__.ignore = True
 
@@ -113,15 +126,15 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
     # data format from allocator: (node_name, ptr_address)
     for allocator_info in __main__.allocator_profiling_info:
         node_name, ptr = allocator_info
-        profiling_info[node_name]["allocator_address"].append(ptr)
+        profiling_info[node_name].add_allocator_address(ptr)
 
     # calculating local indice of each malloc
     local_indice = dict()
     for node_name in profiling_info:
         node_info = profiling_info[node_name]
         local_indice[node_name] = []
-        for i, alloc_addr in enumerate(node_info['allocator_address']):
-            if alloc_addr in node_info['output_address']:
+        for i, alloc_addr in enumerate(node_info.allocator_address):
+            if alloc_addr in node_info.output_address:
                 local_indice[node_name].append(i)
     
     # check whether this node is inplace or not
@@ -129,8 +142,8 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
     for node_name in profiling_info:
         node_info = profiling_info[node_name]
         flag = False
-        for out_addr in node_info['output_address']:
-            if out_addr in node_info['input_address']:
+        for out_addr in node_info.output_address:
+            if out_addr in node_info.input_address:
                 flag = True
                 break
         is_inplace[node_name] = flag
@@ -138,9 +151,9 @@ def allocator_prof(fx_module: torch.fx.GraphModule) -> torch.fx.GraphModule:
     for node_name in profiling_info:
         if local_indice[node_name]:
             pass
-            # print(node_name, profiling_info[node_name]['qualified_name'], local_indice[node_name])
+            print(node_name, profiling_info[node_name].qualified_name, local_indice[node_name])
         elif is_inplace[node_name]:
-            print(node_name, profiling_info[node_name]['qualified_name'], 'inplace')
+            print(node_name, profiling_info[node_name].qualified_name, 'inplace')
         else:
             assert False, "unexpected situation! " + profiling_info[node_name]
 
