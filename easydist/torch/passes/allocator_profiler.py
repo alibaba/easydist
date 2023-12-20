9f627387c533a282273768825826b45ca1951d4f
kuanzhan.wh <kuanzhan.wh@alibaba-inc.com>
2023-12-20 15:26:30 +0800

    [profiling_allocator] move ModuleProfilingInfo, NodeProfilingInfo to allocator_profiler.py

--- easydist/torch/passes/allocator_profiler.py
+++ easydist/torch/passes/allocator_profiler.py
@@ -12,7 +12,7 @@
 # limitations under the License.
 # ==============================================================================
 
-from typing import Any
+from typing import Any, List, Dict
 
 import __main__
 import torch.utils._pytree as pytree
@@ -25,10 +25,90 @@ from torch.fx._compatibility import compatibility
 from easydist.torch.utils import EDNodeType
 from easydist.torch.init_helper import materialize_random
 from easydist.torch.utils import to_meta
-from .allocator_prof import ModuleProfilingInfo, NodeProfilingInfo
 
 __all__ = ['AllocatorProfiler']
 
+
+class NodeProfilingInfo:
+    def __init__(self) -> None:
+        self.qualified_name: str = ""
+        self.input_address: List[int] = []
+        self.output_address: List[int] = []
+        self.allocator_address: List[int] = []
+
+    def set_qualified_name(self, qualified_name: str) -> None:
+        self.qualified_name = qualified_name
+
+    def add_input_address(self, input_address: int) -> None:
+        self.input_address.append(input_address)
+
+    def add_output_address(self, output_address: int) -> None:
+        self.output_address.append(output_address)
+
+    def add_allocator_address(self, allocator_address: int) -> None:
+        self.allocator_address.append(allocator_address)
+
+    def __str__(self) -> str:
+        return_str = self.qualified_name + "\n"
+        return_str += "input_address: " + ",".join([str(addr) for addr in self.input_address]) + "\n"
+        return_str += "output_address: " + ",".join([str(addr) for addr in self.output_address]) + "\n"
+        return_str += "allocator_address: " + ",".join([str(addr) for addr in self.allocator_address]) + "\n"
+
+        return return_str
+
+    def __repr__(self) -> str:
+        return f'NodeProfilingInfo(qualified_name={repr(self.qualified_name)}, ' + \
+            f'input_address={repr(self.input_address)}, ' + \
+            f'output_address={repr(self.output_address)}, ' + \
+            f'allocator_address={repr(self.allocator_address)})'
+
+class ModuleProfilingInfo:
+    def __init__(self) -> None:
+        self.node_profiling_info = dict()
+
+    @property
+    def node_names(self):
+        return self.node_profiling_info.keys()
+
+    def local_indexes(self):
+        return self.local_indexes
+
+    def build_local_indexes(self):
+        # calculating local indexes of each malloc
+        self.local_indexes = dict()
+        for node_name in self.node_names:
+            node_info = self.get_node_profiling_info(node_name)
+            indexes = []
+            for i, alloc_addr in enumerate(node_info.allocator_address):
+                if alloc_addr in node_info.output_address:
+                    indexes.append(i)
+            self.local_indexes[node_name] = indexes
+
+    @property
+    def is_inplace(self):
+        # check whether this node is inplace or not
+        result = dict()
+        for node_name in self.node_names:
+            node_info = self.get_node_profiling_info(node_name)
+            flag = False
+            for out_addr in node_info.output_address:
+                if out_addr in node_info.input_address:
+                    flag = True
+                    break
+            result[node_name] = flag
+        return result
+
+    def set_node_profiling_info(self, node_name, node_info):
+        self.node_profiling_info[node_name] = node_info
+
+    def __setitem__(self, node_name, node_info):
+        self.set_node_profiling_info(node_name, node_info)
+
+    def get_node_profiling_info(self, node_name):
+        return self.node_profiling_info[node_name]
+
+    def __getitem__(self, node_name):
+        return self.get_node_profiling_info(node_name)
 
 @compatibility(is_backward_compatible=True)
 class AllocatorProfiler(Interpreter):
