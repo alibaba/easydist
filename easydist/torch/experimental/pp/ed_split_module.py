7c9918e8bec21d1c2f41cb3bd72b4b01b7930c1a
lansong.dls <lansong.dls@alibaba-inc.com>
2024-06-28 13:49:09 +0800

    [misc] add package tqdm to requirements file

--- easydist/torch/experimental/pp/ed_split_module.py
+++ easydist/torch/experimental/pp/ed_split_module.py
@@ -1,16 +1,3 @@
-# Copyright (c) 2024, Alibaba Group;
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
 # Functions in this file are modified from
 # https://github.com/pytorch/pytorch/blob/5667a990fd0aaa1cec2df8f6666fae9df478ee03/torch/fx/passes/split_module.py#L44
