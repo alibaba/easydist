133767b3c58ea9ccd5a35f3ed2e8f411e365266b
hxwang <wang1570@e.ntu.edu.sg>
2024-07-03 06:01:19 +0000

    [pseudo] gen nd from 1d

--- pseudo.py
+++ pseudo.py
@@ -37,8 +37,8 @@ class DummyFile:
 
 rank = int(os.environ['RANK'])
 world_size = int(os.environ['WORLD_SIZE'])
-global_device_mesh = DeviceMesh(mesh=torch.arange(4).reshape(2, 2), device_type='cpu')
-
+device_mesh_global = DeviceMesh(mesh=torch.arange(4).reshape(2, 2), device_type='cpu')
+device_mesh_1d = DeviceMesh(mesh=device_mesh_global.mesh.flatten(), device_type='cpu')
 
 def reduce(self: torch.Tensor, reduceOp: str, group: List[int], tag: str = ""):
     t = all_reduce_start(self, reduceOp, group, tag)
@@ -61,6 +61,8 @@ def gather(self: torch.Tensor, gather_dim: int, group: List[int], tag: str = "")
     t = all_gather_start(self, gather_dim, group, tag)
     return all_gather_end(t, gather_dim, group, tag)
 
+def identity(self: torch.Tensor, group: List[int]):
+    return self
 
 def getDevAnn(comb_func: partial,
               gather_dim=None) -> Union[Shard, Replicate]:
@@ -80,43 +82,44 @@ def getDevAnn(comb_func: partial,
         assert gather_dim is not None and comb_func.func == gather
         return Shard(gather_dim)
 
-
-def identity(self: torch.Tensor, group: List[int]):
-    return self
-
-
 class TensorDimAnn:
 
-    def __init__(self, device_dims: Optional[Tuple[int]] = None):
+    def __init__(self, device_dims: Optional[List[int]] = None):
+        if isinstance(device_dims, tuple):
+            device_dims = list(device_dims)
         self.device_dims = device_dims
 
     def __repr__(self):
         return "S(" + "".join([str(d)
                                for d in self.device_dims]) + ")" if self.device_dims else "R"
 
+    def __add__(self, other):
+        if self.device_dims is None:
+            return other
+        if other.device_dims is None:
+            return self
+        return TensorDimAnn(self.device_dims + other.device_dims)
 
 TensorAnn = List[TensorDimAnn]
 DeviceAnn = List[Union[Replicate, Shard]]
 CombSeq = List[Callable]
-
 
 class Constant:
 
     def __repr__(self):
         return "C"
 
-
-def getLocalTensor(tensor: Tensor, anns: TensorAnn) -> Tensor:
+def getLocalTensor(tensor: Tensor, anns: TensorAnn, device_mesh: DeviceMesh) -> Tensor:
 
     def to_dtensor_ann(anns: List[TensorDimAnn]):
-        ret = [Replicate() for _ in range(len(global_device_mesh.mesh.shape))]
+        ret = [Replicate() for _ in range(len(device_mesh.mesh.shape))]
         for tensor_dim, ann in enumerate(anns):
             if ann.device_dims:
                 for device_dim in ann.device_dims:
                     ret[device_dim] = Shard(tensor_dim)
         return ret
 
-    dtensor = distribute_tensor(tensor, global_device_mesh, to_dtensor_ann(anns))
+    dtensor = distribute_tensor(tensor, device_mesh, to_dtensor_ann(anns))
     return dtensor._local_tensor
 
 
@@ -124,7 +127,7 @@ def genDimComb(dims: Set[int]) -> List[Tuple[int]]:
     return [x for xx in [combinations(dims, i) for i in range(1, len(dims) + 1)] for x in xx]
 
 
-def searchShardSingle(tensor: Tensor) -> List[TensorAnn]:
+def search1DShardSingle(tensor: Tensor) -> List[TensorAnn]:
     ans = []
 
     def _searchShardSingle(left_dims: Set[int], tensor: Tensor, cur_dim: int,
@@ -145,16 +148,16 @@ def searchShardSingle(tensor: Tensor) -> List[TensorAnn]:
         # no shard
         _searchShardSingle(left_dims, tensor, cur_dim + 1, cur_shard + [TensorDimAnn()])
 
-    _searchShardSingle(set(i for i in range(global_device_mesh.mesh.dim())), tensor, 0, [])
+    _searchShardSingle(set(i for i in range(device_mesh_1d.mesh.dim())), tensor, 0, [])
 
     return ans
 
 
-def getRanks(device_dims: Tuple[int]) -> Tuple[int]:
-    local_coord = deepcopy(global_device_mesh.get_coordinate())
+def getRanks(device_dims: Tuple[int], device_mesh: DeviceMesh) -> Tuple[int]:
+    local_coord = deepcopy(device_mesh.get_coordinate())
     for dim in device_dims:
         local_coord[dim] = slice(None)
-    return global_device_mesh.mesh[tuple(local_coord)].flatten().tolist()
+    return device_mesh.mesh[tuple(local_coord)].flatten().tolist()
 
 
 def getCombFuncs(t: Tensor) -> List[partial]:
@@ -164,7 +167,7 @@ def getCombFuncs(t: Tensor) -> List[partial]:
     return base_funcs
 
 
-def searchCombSingle(comb_funcs: List[partial], device_1d: Tensor=None) -> List[Tuple[CombSeq, DeviceAnn]]:
+def search1DCombSingle(comb_funcs: List[partial]) -> List[Tuple[CombSeq, DeviceAnn]]:
     ans = []
 
     def _searchCombSingle(left_dims: Set[int], cur_comb: CombSeq, cur_dev: DeviceAnn,
@@ -175,9 +178,7 @@ def searchCombSingle(comb_funcs: List[partial], device_1d: Tensor=None) -> List[
 
         for dims in genDimComb(left_dims):
             for comb_func in comb_funcs:
-                # if prev_comb is not None and comb_func.func is prev_comb.func:  # this is redundant since we can use prev comb func with more dims
-                #     continue
-                ranks = getRanks(dims)
+                ranks = getRanks(dims, device_mesh_1d)
                 fn = partial(comb_func, group=ranks)
                 cur_dev_cp = deepcopy(cur_dev)
                 for dim in dims:
@@ -187,15 +188,15 @@ def searchCombSingle(comb_funcs: List[partial], device_1d: Tensor=None) -> List[
                     cur_dev_cp[dim] = dev_ann
                 _searchCombSingle(left_dims - set(dims), cur_comb + [fn], cur_dev_cp, comb_func)
 
-    _searchCombSingle(set(i for i in range(global_device_mesh.mesh.dim())), [],
-                      [Replicate() for _ in range(global_device_mesh.mesh.dim())], None)
+    _searchCombSingle(set(i for i in range(device_mesh_1d.mesh.dim())), [],
+                      [Replicate() for _ in range(device_mesh_1d.mesh.dim())], None)
     return ans
 
 
-def searchSolution(op,
+def search1DSolution(op,
                    op_args: List[Any],
                    args_anns: List[Union[TensorAnn, Constant]],
-                   quick_sol=True) -> List:
+                   quick_sol=True) -> List[Tuple[TensorAnn, CombSeq, DeviceAnn]]:
     global_output = op(*op_args)
     assert isinstance(global_output, Tensor), "only support single output for now"
     op_legit_anns_and_comb = []
@@ -212,7 +213,7 @@ def searchSolution(op,
                 if isinstance(ann, Constant):
                     local_op_args.append(deepcopy(arg))
                     continue
-                local_op_args.append(getLocalTensor(arg, ann))
+                local_op_args.append(getLocalTensor(arg, ann, device_mesh_1d))
                 tensor_ann.append(ann)
 
             def get_global_flag(local_flag: float):
@@ -223,11 +224,11 @@ def searchSolution(op,
             if all(dim_ann.device_dims is None for ann in tensor_ann for dim_ann in ann):
                 if handle_all_replicate_flag:
                     continue
-                # handle_all_replicate_flag = True
-                all_dim = [i for i in range(global_device_mesh.mesh.dim())]
+                handle_all_replicate_flag = True
+                all_dim = [i for i in range(device_mesh_1d.mesh.dim())]
                 op_legit_anns_and_comb.append(
-                    (args_ann, [partial(identity, group=getRanks(all_dim))],
-                     [Replicate() for _ in range(global_device_mesh.mesh.dim())]))
+                    (args_ann, [partial(identity, group=getRanks(all_dim, device_mesh_1d))],
+                     [Replicate() for _ in range(device_mesh_1d.mesh.dim())]))
                 continue
 
             # test op is executable or not (pre-prune)
@@ -244,7 +245,7 @@ def searchSolution(op,
 
             assert isinstance(local_output, Tensor), "only support single output for now"
 
-            possible_combination = searchCombSingle(getCombFuncs(local_output))
+            possible_combination = search1DCombSingle(getCombFuncs(local_output))
 
             for comb_seq in possible_combination:
                 f.write(str(comb_seq) + "\n")
@@ -278,6 +279,63 @@ def searchSolution(op,
 
     return op_legit_anns_and_comb
 
+def searchNDSolution(solutions_1d: List[Tuple[TensorAnn, CombSeq, DeviceAnn]]) -> List[Tuple[TensorAnn, CombSeq, DeviceAnn]]:
+    ans = []
+
+    def _flattenSolutions(cur_sol: List[Tuple[TensorAnn, CombSeq, DeviceAnn]]) -> Tuple[TensorAnn, CombSeq, DeviceAnn]:
+        def _add_args_ann(cur, args_ann):
+            cur = deepcopy(cur)
+            for cur_arg_ann, arg_ann in zip(cur, args_ann):
+                for i, (cur_dim_ann, dim_ann) in enumerate(zip(cur_arg_ann, arg_ann)):
+                    cur_arg_ann[i] = cur_dim_ann + dim_ann
+            return cur
+
+        def _add_comb_seq(comb_seq1, comb_seq2):
+            return comb_seq1 + comb_seq2
+
+        assert len(cur_sol) == device_mesh_global.mesh.dim()
+
+        flatten_sol, *left_sol = cur_sol
+        flatten_sol = deepcopy(flatten_sol)
+        flatten_args_ann, flatten_comb_seq, flatten_dev_ann = flatten_sol
+        assert len(flatten_comb_seq) == 1
+        flatten_comb_seq[0].keywords['group'] = getRanks((0, ), device_mesh_global)
+
+        cur_device_dim = 1
+        for tmp in left_sol:
+            args_ann, comb_seq, _ = deepcopy(tmp)
+            for arg_ann in args_ann:
+                for dim_ann in arg_ann:
+                    if dim_ann.device_dims is not None:
+                        assert len(dim_ann.device_dims) == 1, f"{dim_ann=}"
+                        dim_ann.device_dims = [cur_device_dim]
+            flatten_args_ann = _add_args_ann(flatten_args_ann, args_ann)
+            assert len(comb_seq) == 1
+            comb_seq[0].keywords['group'] = getRanks((cur_device_dim, ), device_mesh_global)
+            flatten_comb_seq = _add_comb_seq(flatten_comb_seq, comb_seq)
+            cur_device_dim += 1
+
+        if rank == 0:
+            print(f"{flatten_args_ann=} {flatten_comb_seq=}")
+
+        flatten_dev_ann = [getDevAnn(comb_func, gather_dim= comb_func.keywords['gather_dim'] if 'gather_dim' in comb_func.keywords else None) for comb_func in flatten_comb_seq]
+
+        return flatten_args_ann, flatten_comb_seq, flatten_dev_ann
+
+
+    def _searchNDSolution(device_dim: int, cur_sol: List):
+        if device_dim >= device_mesh_global.mesh.dim():
+            ans.append(_flattenSolutions(cur_sol))
+            return
+
+        for sol in solutions_1d:
+            cur_sol.append(sol)
+            _searchNDSolution(device_dim + 1, cur_sol)
+            cur_sol.pop()
+
+    _searchNDSolution(0, [])
+    return ans
+
 
 def searchShard(arg_ann: List[Union[List[TensorDimAnn], Constant]]):
     tensor_ann = []
@@ -309,7 +367,7 @@ def searchShard(arg_ann: List[Union[List[TensorDimAnn], Constant]]):
 
 def test_single_tensor_shard():
     tensor = torch.ones(16, 4, 8)
-    shardAnn = searchShardSingle(tensor)
+    shardAnn = search1DShardSingle(tensor)
     if rank == 0:
         print(len(shardAnn))
         for ann in shardAnn:
@@ -319,15 +377,16 @@ def test_single_tensor_shard():
 def test_matmul():
     tensor1 = torch.rand(32, 16, 8)
     tensor2 = torch.rand(32, 8, 16)
-    shardAnn1 = searchShardSingle(tensor1)
-    shardAnn2 = searchShardSingle(tensor2)
+    shardAnn1 = search1DShardSingle(tensor1)
+    shardAnn2 = search1DShardSingle(tensor2)
     args_combs = searchShard([shardAnn1, shardAnn2])
-    ans = searchSolution(torch.matmul, [tensor1, tensor2], args_combs)
+    ans = search1DSolution(torch.matmul, [tensor1, tensor2], args_combs)
+    ans = searchNDSolution(ans)
     if rank == 0:
-        with open(f'device{tuple(global_device_mesh.mesh.shape)}-matmul_sol.txt', 'w') as f:
+        with open(f'device{tuple(device_mesh_global.mesh.shape)}-matmul_sol.txt', 'w') as f:
             f.write(f"ans len: {len(ans)}\n")
             for x in ans:
-                f.write(f"input spec: {str(x[0]):<40}output DTensor spec: {str(x[2]):<60}, combine funcs: {str(x[1]):>100}\n")
+                f.write(f"input spec: {str(x[0]):<40}output DTensor spec: {str(x[2]):<60}\n")
 
 
 @torch.no_grad
@@ -335,22 +394,23 @@ def test_conv():
     layer = torch.nn.Conv2d(32, 64, 8, stride=1)
     tensor = torch.rand(16, 32, 32, 32)
     weight, bias, stride, padding, dilation, groups = layer.weight, layer.bias, layer.stride, layer.padding, layer.dilation, layer.groups
-    tensorAnn = searchShardSingle(tensor)
-    weightAnn = searchShardSingle(weight)
-    biasAnn = searchShardSingle(bias)
+    tensorAnn = search1DShardSingle(tensor)
+    weightAnn = search1DShardSingle(weight)
+    biasAnn = search1DShardSingle(bias)
 
     args_combs = searchShard(
         [tensorAnn, weightAnn, biasAnn,
          Constant(), Constant(),
          Constant(), Constant()])
 
-    ans = searchSolution(torch.conv2d, [tensor, weight, bias, stride, padding, dilation, groups],
+    ans = search1DSolution(torch.conv2d, [tensor, weight, bias, stride, padding, dilation, groups],
                          args_combs)
+    ans = searchNDSolution(ans)
     if rank == 0:
-        with open(f'device{tuple(global_device_mesh.mesh.shape)}-conv_sol.txt', 'w') as f:
+        with open(f'device{tuple(device_mesh_global.mesh.shape)}-conv_sol.txt', 'w') as f:
             f.write(f"ans len: {len(ans)}\n")
             for x in ans:
-                f.write(f"input spec: {str(x[0]):<50}output DTensor spec: {str(x[2]):<60}, combine funcs: {str(x[1]):>100}\n")
+                f.write(f"input spec: {str(x[0]):<60}output DTensor spec: {str(x[2]):<60}, combine funcs: {str(x[1])}\n")
 
 
 
@@ -372,4 +432,4 @@ def seed(seed=42):
 if __name__ == '__main__':
     seed()
     test_matmul()
-    test_conv()
+    # test_conv()
